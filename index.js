11de023edaa07f22ecc152936320f4531eb30c08
lobana alarabi <loubanaeng@gmail.com>
2020-01-28 15:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function fetchBooks() {
 return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
@@ -16,3 +17,8 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+=======
+function fetchBooks() {  return fetch('https://anapioficeandfire.com/api/books')  .then(resp => resp.json())  .then(json => renderBooks(json));}
+function renderBooks(json) {  const main = document.querySelector('main')  json.forEach(book => {    const h2 = document.createElement('h2')    h2.innerHTML = `<h2>${book.name}</h2>`    main.appendChild(h2)  })}
+document.addEventListener('DOMContentLoaded', function() {  fetchBooks()})
+>>>>>>> 519fa9c721ff19add517273220d75bfa7383cd0a
